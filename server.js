d9607def9e6f633ef4cba41cbbaa785cab2ef365
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-27 18:36:06 +0000

    added welcome message route

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var db = require('./db');
 
 
 
+
+app.get('/', function(req,res){
+    res.send('Welcome to Hashtask');
+})
+
+
 db.connect().then(function(){
     app.listen(PORT, function(){
         console.log("Listening on port " + PORT + "...");
