2a02e3aadf98e977c4a906f12ddf8170890ca7d5
Cathal Killeen <cathalkilleen@gmail.com>
2016-02-27 21:18:38 +0000

    added all todo routes

--- routes/todos.js
+++ routes/todos.js
@@ -27,4 +27,71 @@ router.get('/', function(req, res){
         })
 })
 
+router.get('/:id', function(req,res){
+    var where = {
+        _id: req.params.id
+    }
+
+    db.todo.findOne(where)
+        .then(function(todo){
+            if(!!todo){
+                return res.status(200).json(todo.toJSON()).send();
+            }else{
+                return res.status(404).send();
+            }
+        }).catch(function(e){
+            return res.status(500).json(e).send();
+        })
+})
+
+router.delete('/:id', function(req,res){
+    var where = {
+        _id: req.params.id
+    }
+
+    db.todo.remove(where)
+        .then(function(WriteResult){
+            console.log(WriteResult);
+            if(WriteResult.result.n === 0){
+                return res.status(404).json({
+                    error: "No todo with id " + req.params.id
+                }).send();
+            }else{
+                return res.status(204).send();
+            }
+        }).catch(function(e){
+            return res.status(500).json(e).send();
+        })
+})
+
+router.put('/:id', function(req,res){
+    var body = _.pick(req.body, 'description', 'completed');
+    var attributes = {};
+
+    if(body.hasOwnProperty('completed')){
+        attributes.completed = body.completed;
+    }
+    if(body.hasOwnProperty('description')){
+        attributes.description = body.description;
+    }
+
+    var where = {
+        _id: req.params.id
+    }
+
+    db.todo.findOne(where).then(function(todo){
+        if(todo){
+            todo.update(attributes).then(function(todo){
+                res.json(todo.toJSON());
+            }, function(e){
+                res.status(400).json(e).send();
+            })
+        }else{
+            res.status(404).send();
+        }
+    }, function(){
+        res.status(500).send();
+    })
+})
+
 module.exports = router;
